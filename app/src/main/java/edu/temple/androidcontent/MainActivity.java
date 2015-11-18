c2558dee882a50c64977e92442e158664f4822e7
Karl Morris <siliclone@gmail.com>
2015-11-18 16:02:57 -0500

    Fetching stock data from a content provider.

--- app/src/main/java/edu/temple/androidcontent/MainActivity.java
+++ app/src/main/java/edu/temple/androidcontent/MainActivity.java
@@ -1,17 +1,17 @@
 package edu.temple.androidcontent;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,7 +21,9 @@ public class MainActivity extends Activity {
     Cursor cursor;
 
     TextView contactName;
+    EditText symbolEditText;
 
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,84 +35,30 @@ public class MainActivity extends Activity {
 
 
         // Check for permission to read contacts and get cursor
-        if (checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
+        if ((android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
             requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 1234);
-        else
-            cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-
 
         contactName = (TextView) findViewById(R.id.nameTextView);
+        symbolEditText = (EditText) findViewById(R.id.symbolEditText);
 
         // Step to next item in cursor
-        findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.searchButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean success = cursor.moveToNext();
+
+
+                cursor = cr.query(Uri.parse("content://edu.temple.androidcontentprovider.provider.STOCK_DATA")
+                        , null
+                        , "symbol=?"
+                        , new String[]{symbolEditText.getText().toString()}
+                        , null);
+
+                boolean success = cursor.moveToFirst();
                 if (success)
-                    contactName.setText(cursor.getString(
-                            cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)
-                    ));
+                    contactName.setText(cursor.getString(1));
             }
         });
 
-
-        // Step to previous item in cursor
-        findViewById(R.id.backButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                boolean success = cursor.moveToPrevious();
-                if (success)
-                    contactName.setText(cursor.getString(
-                            cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)
-                    ));
-            }
-        });
-
-
-
-        // Create cursor adapter and populate ListView
-        findViewById(R.id.allButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ListView contactsList = (ListView) findViewById(R.id.contactsListView);
-
-                // Columns to display
-                String[] FROM_COLUMNS = {ContactsContract.Contacts.DISPLAY_NAME_PRIMARY};
-
-                // View ids to display respective column
-                int[] TO_IDS = {android.R.id.text1};
-
-                SimpleCursorAdapter contactsCurser = new SimpleCursorAdapter(
-                        MainActivity.this,
-                        R.layout.contact_list_item,
-                        cursor,
-                        FROM_COLUMNS, TO_IDS,
-                        0);
-                ;
-
-                contactsList.setAdapter(contactsCurser);
-
-            }
-        });
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -120,7 +68,7 @@ public class MainActivity extends Activity {
         if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
             cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
         else {
-            Toast.makeText(MainActivity.this, "This app cannot function without access to your contacts", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "This app cannot function without access to the data source", Toast.LENGTH_LONG).show();
             finish();
         }
     }
